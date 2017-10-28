86b320f4ad9c7734d2421724b2651f97a7c9f588
Oleksandr Chzhen <Oleksandr_Chzhen@epam.com>
2017-10-28 13:20:26 +0300

    Create left-side-nav and footer-nav

---
+++ src/app/footer-nav/footer-nav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'footer-nav',
+    templateUrl: './footer-nav.component.html'
+})
+export class FooterNavComponent {
+
+}
---
+++ src/app/left-side-nav/left-side-nav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'left-side-nav',
+    templateUrl: './left-side-nav.component.html'
+})
+export class LeftSideNavComponent {
+
+}
